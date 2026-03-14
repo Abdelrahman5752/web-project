07aa53ec1b6ff90e00ff7388bd7ec4cafd98ac4b
Abdelrahman <abdelrhmann424@gmail.com>
2026-03-14 22:44:27 +0200

    Implement dark mode toggle and loader display
    Add dark mode toggle functionality and loader handling.

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
-function toggleMode(){
+/* تحميل الوضع المحفوظ */
 
-document.body.classList.toggle("dark")
+window.onload = function(){
+
+let mode = localStorage.getItem("mode");
+
+const btn = document.getElementById("modeBtn");
+
+if(mode === "dark"){
+
+document.body.classList.add("dark");
+btn.innerHTML = "☀️";
+
+}else{
+
+btn.innerHTML = "🌙";
 
 }
+
+}
+
+
+
+
+function toggleMode(){
+
+document.body.classList.toggle("dark");
+
+const btn = document.getElementById("modeBtn");
+
+if(document.body.classList.contains("dark")){
+
+btn.innerHTML = "☀️";
+localStorage.setItem("mode","dark");
+
+}else{
+
+btn.innerHTML = "🌙";
+localStorage.setItem("mode","light");
+
+}
+
+}
+window.addEventListener("load", function(){
+
+document.getElementById("loader").style.display="none";
+
+});
+particlesJS("particles-js",{
+
+particles:{
+
+number:{value:80},
+
+size:{value:3},
+
+move:{speed:2},
+
+line_linked:{
+enable:true
+}
+
+}
+
+});
