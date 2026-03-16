7a559bb48d4eb3e09cf2175d97f0cae1c10df3e9
Abdelrahman <abdelrhmann424@gmail.com>
2026-03-16 19:16:45 +0200

    Implement pupil movement and eye open/close functions
    Add pupil movement effect on mouse move and eye control functions

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-/* تحميل الوضع المحفوظ */
+
+const pupils = document.querySelectorAll(".pupil")
+
+document.addEventListener("mousemove",(e)=>{
+
+pupils.forEach(pupil=>{
+
+let rect = pupil.parentElement.getBoundingClientRect()
+
+let x = e.clientX - rect.left - rect.width/2
+let y = e.clientY - rect.top - rect.height/2
+
+pupil.style.transform = `translate(${x/10}px,${y/10}px)`
+
+})
+
+})
+
+function closeEyes(){
+
+document.querySelectorAll(".eye").forEach(eye=>{
+eye.classList.add("closed")
+})
+
+}
+
+function openEyes(){
+
+document.querySelectorAll(".eye").forEach(eye=>{
+eye.classList.remove("closed")
+})
+
+}
 
 window.onload = function(){
 
