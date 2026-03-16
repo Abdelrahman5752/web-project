8b70cef26161f03377dbdb1d6d51564d894637cf
Abdelrahman <abdelrhmann424@gmail.com>
2026-03-17 00:01:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,97 @@
-/* في ملف script.js */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+
+const pupils = document.querySelectorAll(".pupil")
+
+document.addEventListener("mousemove",(e)=>{
+
+pupils.forEach(pupil=>{
+
+let rect = pupil.parentElement.getBoundingClientRect()
+
+let x = e.clientX - rect.left - rect.width/2
+let y = e.clientY - rect.top - rect.height/2
+
+pupil.style.transform = `translate(${x/10}px,${y/10}px)`
+
+})
+
+})
+
+function closeEyes(){
+
+document.querySelectorAll(".eye").forEach(eye=>{
+eye.classList.add("closed")
+})
+
+}
+
+function openEyes(){
+
+document.querySelectorAll(".eye").forEach(eye=>{
+eye.classList.remove("closed")
+})
+
+}
+
+window.onload = function(){
+
+let mode = localStorage.getItem("mode");
+
+const btn = document.getElementById("modeBtn");
+
+if(mode === "dark"){
+
+document.body.classList.add("dark");
+btn.innerHTML = "☀️";
+
+}else{
+
+btn.innerHTML = "🌙";
+
+}
+
+}
+
+
+
+
+function toggleMode(){
+
+document.body.classList.toggle("dark");
+
+const btn = document.getElementById("modeBtn");
+
+if(document.body.classList.contains("dark")){
+
+btn.innerHTML = "☀️";
+localStorage.setItem("mode","dark");
+
+}else{
+
+btn.innerHTML = "🌙";
+localStorage.setItem("mode","light");
+
+}
+
+}
+window.addEventListener("load", function(){
+
+document.getElementById("loader").style.display="none";
+
 });
-const canvas = document.getElementById("matrix");
+particlesJS("particles-js",{
 
-if (canvas) {
-  const ctx = canvas.getContext("2d");
+particles:{
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+number:{value:80},
 
-  const letters = "01";
-  const fontSize = 16;
-  const columns = Math.floor(canvas.width / fontSize);
-  const drops = Array(columns).fill(1);
+size:{value:3},
 
-  function drawMatrix() {
-    ctx.fillStyle = "rgba(0,0,0,0.05)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+move:{speed:2},
 
-    ctx.fillStyle = "#00ff9d";
-    ctx.font = fontSize + "px monospace";
+line_linked:{
+enable:true
+}
 
-    for (let i = 0; i < drops.length; i++) {
-      const text = letters[Math.floor(Math.random() * letters.length)];
-      ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+}
 
-      if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
-        drops[i] = 0;
-      }
-
-      drops[i]++;
-    }
-  }
-  setInterval(drawMatrix, 35);
-  
-// Responsive
-window.addEventListener("resize", () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
 });
